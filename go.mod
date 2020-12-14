c7a2fea46395a94e572194e68291f81e9f6c6d15
Asthetic <jordoncain93@gmail.com>
2020-12-13 21:34:51 -0800

    Add ticker loop, handle system signals

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/Asthetic/DiscordGameServerBot
 
 go 1.15
 
-require github.com/pkg/errors v0.9.1
+require (
+	github.com/bwmarrin/discordgo v0.22.0
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.7.0
+)
--- main.go
+++ main.go
@@ -1,27 +1,61 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/Asthetic/DiscordGameServerBot/network"
 	"github.com/Asthetic/DiscordGameServerBot/storage"
+	"github.com/bwmarrin/discordgo"
+	log "github.com/sirupsen/logrus"
 )
 
+// Session is our global discord session
+var Session, _ = discordgo.New()
+
 func main() {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+
+	ticker := time.NewTicker(5 * time.Minute)
+	defer ticker.Stop()
+
+	done := make(chan bool)
+
+	for {
+		getIP()
+
+		select {
+		case <-done:
+			return
+		case s := <-sig:
+			log.Infof("Got signal to kill program: %v", s)
+			return
+		case <-ticker.C:
+			getIP()
+		}
+	}
+}
+
+func getIP() {
 	currentIP, err := storage.GetIP()
 	if err != nil {
-		fmt.Printf("error fetching file: %v", err)
+		log.WithError(err).Errorf("Error fetching file: %v", err)
 	}
 
 	ip, err := network.GetPublicIP()
 	if err != nil {
-		fmt.Printf("error getting IP address: %v", err)
+		log.WithError(err).Errorf("Error getting IP address: %v")
 	}
 
 	if currentIP != ip {
 		currentIP = ip
-		storage.WriteIP(network.Network{IP: currentIP})
+		err = storage.WriteIP(network.Network{IP: currentIP})
+		if err != nil {
+			log.WithError(err).Errorf("Error writing IP to local storage")
+		}
 	}
 
-	fmt.Printf("%s", currentIP)
+	log.Infof("%s", currentIP)
 }
